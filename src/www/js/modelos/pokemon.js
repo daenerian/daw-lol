ec653aea6e6afb4107ebaa49a896ee32805fb454
Daniel Carvajal <juandanielcarvajalmontes.guadalupe@alumnado.fundacionloyola.net>
2023-01-09 14:13:36 +0100

    Establecimiento de la estructura de directorios

---
+++ src/www/js/modelos/pokemon.js
@@ -0,0 +1,15 @@
+class Pokemon{
+
+    constructor() {
+        this.nombre = null;
+        this.numPokedex = null;
+        this.descripcion = null;
+        this.fechaAparicion = null;
+        this.creado = null;
+        this.clasificacion = ['Inicial', 'Normal', 'Legendario', 'Mitico'];
+        this.tipo = ['Fuego', 'Agua', 'Planta', 'Tierra', 'Roca', 'Hada', 'Dragón', 'Acero', 'Normal', 'Lucha', 'Siniestro', 'Psiquico', 'Bicho', 'Electrico', 'Fantasma',
+                    'Hielo', 'Volador', 'Veneno'];
+        this.imagen = null;
+    }
+
+}
